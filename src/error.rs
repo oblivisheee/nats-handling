8eed077d1fc48754b70a242a75294b4bcb7cc61f
oblivisheee <molniya213y@gmail.com>
2025-05-02 22:58:39 +0300

    error improvements

--- src/error.rs
+++ src/error.rs
@@ -2,8 +2,8 @@ use async_nats::{Error as NatsError, UnsubscribeError};
 use thiserror::Error;
 #[derive(Error, Debug)]
 pub enum Error {
-    #[error("NATS error: {0}")]
+    #[error("{0}")]
     NatsError(#[from] NatsError),
-    #[error("Failed to unsubscribe: {0}")]
+    #[error("Unsubscribe error: {0}")]
     UnsubscribeError(#[from] UnsubscribeError),
 }
